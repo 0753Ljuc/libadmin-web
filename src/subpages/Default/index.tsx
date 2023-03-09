a315b0ca18302171358d447877f07e3354dbd788
limeQuery <limequery@163.com>
2023-03-09 09:37:27 +0800

    fix: assets path change

--- src/subpages/Default/index.tsx
+++ src/subpages/Default/index.tsx
@@ -5,7 +5,7 @@ const Default: React.FC = () => {
 
   return (
     <div>
-      <Image src='/public/cover_1.jpg' preview={false} />
+      <Image src='/cover_1.jpg' preview={false} />
     </div>
   )
 }
